18993de7659cd056ab5fa73bdb5296346e884c9b
Tamara Temple <tamouse@gmail.com>
2016-05-21 17:20:42 -0500

    add more style, 404 page

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<main class="container">
+    <div class="jumbotron bg-danger">
+        <h1>Oops!</h1>
+        <p>How did we get here?</p>
+    </div>
+</main>
+<?php get_footer(); ?>
+
