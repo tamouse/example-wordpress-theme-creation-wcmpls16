7e16f9e8f6952233425cd18de07bb3f4fe5e7f59
Tamara Temple <tamouse@gmail.com>
2016-05-21 19:02:00 -0500

    make a cool 404 page

--- 404.php
+++ 404.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
 <main class="container">
-    <div class="jumbotron bg-danger">
+    <div class="jumbotron jumbotron-danger">
         <h1>Oops!</h1>
         <p>How did we get here?</p>
+        <p><a class="btn btn-primary"
+                href="/blog/">Go back to the beginning.</a></p>
     </div>
 </main>
 <?php get_footer(); ?>
